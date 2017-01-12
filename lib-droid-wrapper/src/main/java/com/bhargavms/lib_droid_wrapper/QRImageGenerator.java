048de7ec5cf4813bee0d58bde9e6096022b12bc2
Bhargav MS <fcbm18343@fcblr-m0168.ad.fc.net>
2017-01-12 16:40:19 +0530

    Ad lib-droid-wrapper

---
+++ lib-droid-wrapper/src/main/java/com/bhargavms/lib_droid_wrapper/QRImageGenerator.java
@@ -0,0 +1,49 @@
+package com.bhargavms.lib_droid_wrapper;
+
+import android.graphics.Bitmap;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.common.BitMatrix;
+
+import static android.graphics.Color.BLACK;
+import static android.graphics.Color.WHITE;
+
+/**
+ * Qr code image generator.
+ */
+public class QRImageGenerator {
+    /**
+     * Encode the given string to a QR format Bitmap.
+     *
+     * @param stringToEncode  the string that needs to be encoded into QR format
+     * @param preferredWidth  the preferred width in pixels of the Generated Bitmap
+     * @param preferredHeight the preferred height in pixels of the Generated Bitmap
+     * @return the generated QR formatted Bitmap.
+     */
+    public Bitmap encodeAsBitmap(String stringToEncode, int preferredWidth, int preferredHeight) {
+        BitMatrix result;
+        try {
+            result = new MultiFormatWriter()
+                    .encode(stringToEncode, BarcodeFormat.QR_CODE, preferredWidth, preferredHeight, null);
+        } catch (Exception iae) {
+            // Unsupported format
+            iae.printStackTrace();
+            return null;
+        }
+
+        int width = result.getWidth();
+        int height = result.getHeight();
+        int[] pixels = new int[width * height];
+        for (int y = 0; y < height; y++) {
+            int offset = y * width;
+            for (int x = 0; x < width; x++) {
+                pixels[offset + x] = result.get(x, y) ? BLACK : WHITE;
+            }
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+        return bitmap;
+    }
+}
